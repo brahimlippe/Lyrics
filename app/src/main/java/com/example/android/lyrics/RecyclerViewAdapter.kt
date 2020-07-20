83aae6ffd01cf082b2d875889cff073ddee52493
Brahim SAHBI <brahim.sahbi@gmail.com>
2020-07-20 15:02:49 +0200

    Remove useless Log

--- app/src/main/java/com/example/android/lyrics/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/android/lyrics/RecyclerViewAdapter.kt
@@ -13,7 +13,6 @@ class RecyclerViewAdapter(
     class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        Log.i("RecyclerViewAdapter", "onCreateViewAdapter called")
         val textView = LayoutInflater.from(parent.context)
             .inflate(R.layout.one_result, parent, false) as TextView
         return ViewHolder(textView)
@@ -22,15 +21,13 @@ class RecyclerViewAdapter(
     override fun getItemCount(): Int = viewModel.listOfSongs.value?.size ?: 0
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Log.i("RecyclerViewAdapter", "onBindViewAdapter called")
         if (viewModel.listOfSongs.value == null) {
             // This case seems impossible
-            Log.i("RecyclerViewAdapter", "No songs found")
+            Log.e("RecyclerViewAdapter", "No songs found")
             return
         }
         holder.textView.text = viewModel.listOfSongs.value!![position].title
         holder.textView.setOnClickListener {
-            Log.d("RecyclerViewAdapter", "Updating lyrics view model")
             viewModel.lyrics.value = viewModel.listOfSongs.value!![position].lyrics
         }
     }
